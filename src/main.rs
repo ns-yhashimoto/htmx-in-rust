cdc1bcac084b26f25c03037cbe577d2aac4b16b4
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2024-07-01 13:41:51 +0000

    feat: add Tera features

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use tera::{Context, Tera};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let tera = match Tera::new("templates/**/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let ctx = Context::new();
+    let t = tera.render("index.html", &ctx).unwrap();
+    
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(t)
 }
 
 #[post("/echo")]
