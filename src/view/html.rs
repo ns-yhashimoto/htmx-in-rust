b21d704d44ea926a8b1fc01e044d492cc2c51207
kurunin52 <33592045+kurunin52@users.noreply.github.com>
2024-07-05 14:29:15 +0000

    feat: add current nav identifier

--- src/view/html.rs
+++ src/view/html.rs
@@ -1,7 +1,8 @@
 pub mod root {
     use tera::{Context, Tera};
     pub fn render_index_page(tera: &Tera) -> String {
-        let ctx = Context::new();
+        let mut ctx = Context::new();
+        ctx.insert("page", "/");
         tera.render("index.html", &ctx).unwrap()
     }
 }
@@ -11,6 +12,7 @@ pub mod order {
     use crate::model::order::OrderBalance;
     pub fn render_index_page(tera: &Tera, orders: &Vec<OrderBalance<'static>>) -> String {
         let mut ctx = Context::new();
+        ctx.insert("page", "/order");
         ctx.insert("orders", orders);
         tera.render("order/index.html", &ctx).unwrap()
     }
